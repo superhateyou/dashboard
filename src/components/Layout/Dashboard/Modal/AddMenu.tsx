a255a9f10f8f8619f642d8b3ca54ea1eb7b23403
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-05-08 19:11:57 +0300

    fix: fix

--- src/components/Layout/Dashboard/Modal/AddMenu.tsx
+++ src/components/Layout/Dashboard/Modal/AddMenu.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { getWidgets } from '../../../../api/api';
